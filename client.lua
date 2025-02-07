0ec1dc3bc872707866f947beafc3757a95c950e4
Guardian Labs <austingbball3@icloud.com>
2025-02-07 06:05:40 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,18 +1,27 @@
-Citizen.CreateThread(function(source)
+Citizen.CreateThread(function()
     while true do
         local player = GetPlayerPed(-1)
         
         Citizen.Wait(1000)
 
+        -- Set Discord App ID
         SetDiscordAppId(YourAPPID)
-		local onlinePlayers = GetNumberOfPlayers()
-        SetRichPresence(GetNumberOfPlayers().."/10")
 
+        -- Set player count display
+        local onlinePlayers = GetNumberOfPlayers()
+        SetRichPresence(onlinePlayers.."/48")
+
+        -- Set Large Image
         SetDiscordRichPresenceAsset("large")
-        SetDiscordRichPresenceAssetText("Your Server Name")
+        SetDiscordRichPresenceAssetText("YOURSERVER")
 
+        -- Set Small Image
         SetDiscordRichPresenceAssetSmall("min") 
-        SetDiscordRichPresenceAssetSmallText("Whatever you want to put here...")
+        SetDiscordRichPresenceAssetSmallText("WHATEVERYOUWANTHERE")
+
+        -- Add Buttons (Max 2)
+        SetDiscordRichPresenceAction(0, "Discord", "https://discord.gg/yourdc")
+        SetDiscordRichPresenceAction(1, "Connect to Server", "fivem://connect/yourserverip")
 
     end
 end)
